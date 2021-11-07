1de682dd2ac7957fc07bc4374925f819483c482d
s24289 <92215237+s24289@users.noreply.github.com>
2021-11-07 12:31:07 +0100

    Add files via upload

---
+++ Task3_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+#include <string>
+
+//Napisz program, wyswietlajacy na ekranie znaki z klawiatury do momentu, az zostanie podany znak "t".
+
+
+using std::cout;
+using std::endl;
+int main()
+{
+	int operacja = 1;
+	char a = ' ';
+
+	cout << "Pisz do woli!" << endl << endl;
+
+	while (operacja) {
+
+		cout << "Wcisnij jakis klawisz" << endl;
+		a = _getch();
+		cout << "Wpisales: " << a << endl;
+		if (a == 't' || a == 'T')
+		{
+			break;
+		}
+	}
+}
